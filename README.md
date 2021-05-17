d4c5a39a419e6e65440f01776999932331aa8b17
Den Ivanov <securityxiii@gmail.com>
2021-05-17 17:49:42 +0200

    Pushing files for the first time

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# chia-log-analyzer
+### Chia log analyzer
 Bash script that post-processes chia plotter's logs to see your statistics
+
+### Description
+Have you ever considered what's your best plotting options? Forget to write down some values during plotting? That's no longer a problem due to this script.
+
+### Requirements
+* Linux
+* Bash and standard CLI tools installed [awk, grep] (most of Linux distros has this installed already)
+* Already existing log files from chia plotting
+
+### What is inside this repo
+* `chiacheck_all.sh` - Script that builds statistics from all files, both from finished and cancelled plots.
+* `chiacheck_onlyfinished.sh` - Script that builds statistics from only finished plots.
+* `README.md` - this file (the one that you're reading now)
+* `LICENSE` - Copy of MIT license file.
+
+Note: no demo files are provided. The script won't work as-is unless copied to Chia Plotter Log directory.
+
+### How to use
+This script (without modifications) should reside in plotter's log directory, which by default can be found here: `~/.chia/mainnet/plotter/`.
+If your path differs from this, please change the clone command accordingly.
+
+1. Clone this repo right into your plotter directory: `git clone https://github.com/sxiii/chia-log-analyzer ~/.chia/mainnet/plotter/`
+2. Enter the directory: `cd ~/.chia/mainnet/plotter/`
+3. Run the script (choose one of the two above, for example: `./chiacheck_onlyfinished.sh`)
+4. Open your terminal to full screen. Wait several seconds. You would see results on the screen as well as in file `result.tsv`.
+5. Optional: if you want, open the file (result.tsv) with any spreadsheet program you like, for example: `gnumeric result.tsv` or `libreoffice result.tsv` or `openoffice result.tsv` or others, or even just show everything again in terminal: `cat result.tsv`.
+
---
+++ chiacheck_onlyfinished.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# ..:: CHIACHECK ::.. by Security XIII / Written on 17 may 2021         #
+# Script that checks your chia logs and shows you arranged statistics   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+plots="plotter_log_*txt"
+totalnumber=$(ls $plots | wc -l)
+echo "Total number of plot log files found: $totalnumber"
+
+printf 'Filename & Hash .................................... \t TC \t RAM ... \t Stage 1 \t Stage 2 \t Stage 3 \t Stage 4 \t Total Time \t Copy time \t Date started .......... \t Date completed \n';
+
+# Go around all text files in directory
+for filename in *txt; do
+
+	threads=$(cat $filename | grep threads | awk -F" " '{ print $2 }')
+	ram=$(cat $filename | grep "Buffer size" | awk -F" " '{ print $4 }')
+	stage1=$(cat $filename | grep "Time for phase 1" | awk -F" " '{ print $6 }')
+	stage2=$(cat $filename | grep "Time for phase 2" | awk -F" " '{ print $6 }')
+	stage3=$(cat $filename | grep "Time for phase 3" | awk -F" " '{ print $6 }')
+	stage4=$(cat $filename | grep "Time for phase 4" | awk -F" " '{ print $6 }')
+	totaltime=$(cat $filename | grep "Total time" | awk -F" " '{ print $4 }')
+	copytime=$(cat $filename | grep "Copy time" | awk -F" " '{ print $4 }')
+	datestart=$(head -1 $filename | awk -F" " '{ print $1 }')
+	dateend=$(tail -1 $filename | awk -F" " '{ print $1 }' | grep 20) 
+	# Grep 20 to filter only rows which has year in them. If there is no year in lasw row, it means the script was cancelled
+	# Now we save our data to file
+	if [ -z "$dateend" ]; then (( x++ )); else printf "$filename \t $threads \t $ram \t $stage1 \t $stage2 \t $stage3 \t $stage4 \t $totaltime \t $copytime \t $datestart \t $dateend \n" >> result.tsv; fi
+	
+done
+
+cat result.tsv
+
+echo "Skipped $x files because plotting process was cancelled (wasn't finished completely)"
+
+# If you want to keep the resulting file, comment-out the next line
+# If you want to remove the resulting file, uncommet the next line
+# rm result.tsv
+
